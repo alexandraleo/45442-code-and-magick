0c8c30e1b574b6aad4f99e83ec45e8ce0693c3e7
Alexandra Leonova <kabrenok@gmail.com>
2017-11-21 03:39:45 +0300

    Правки по функциям отрисовки на канвасе

--- js/stat.js
+++ js/stat.js
@@ -3,57 +3,72 @@
 var getRandom = function (number) {
   return Math.random() * number;
 };
-var getSorted = function (array) {
+var sort = function (array) {
   array.sort(function (a, b) {
     return a[0] - b[0];
   });
 };
+var colorAlpha = function (r, g, b, a) {
+  return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + a + ')';
+};
 
 window.renderStatistics = function (ctx, names, times) {
+  var drawText = function (array, x, y, indent) {
+    for (var i = 0; i < array.length; i++) {
+      ctx.fillText(array[i], x, y + i * indent);
+    }
+  };
 
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
-  ctx.fillStyle = 'rgb(255, 255, 255)';
-  ctx.fillRect(100, 10, 420, 270);
+  var drawBar = function (barX, barY, barWidth, barHeight, color, time, name) {
+    var bottomBarPadding = 265;
+    if (name === ('Вы')) {
+      ctx.fillStyle = heroColor;
+    } else {
+      ctx.fillStyle = colorAlpha(0, 0, 255, getRandom(1));
+    }
+    ctx.fillRect(barX, barY, barWidth, barHeight);
+    ctx.fillStyle = colorAlpha(0, 0, 0, 1);
+    ctx.textAlign = 'center';
+    ctx.fillText(time, barX + barWidth / 2, barY - 10);
+    ctx.fillText(name, barX + barWidth / 2, bottomBarPadding);
+  };
+  var results = [];
+  var collectResults = function () {
+    for (var i = 0; i <= names.length - 1; i++) {
+      results.push([times[i], names[i]]);
+    }
+  };
+  var drawDiagram = function () {
+    for (var i = 0; i <= names.length - 1; i++) {
+      var name = results[i][1];
+      var time = results[i][0];
 
+      var barX = leftPadding + (barGap + barWidth) * i;
+      var itemHeight = Math.floor(barHeight * time / maxTime);
+      var barY = topBarPadding + (barHeight - itemHeight);
+      var timeInSec = Math.floor(time) + 'мс';
+      drawBar(barX, barY, barWidth, itemHeight, colorAlpha, timeInSec, name);
+    }
+  };
   var leftPadding = 120;
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = 'rgb(0, 0, 0)';
-  ctx.fillText('Ура, вы победили!', leftPadding, 50);
-  ctx.fillText('Список результатов:', leftPadding, 70);
-
+  var topPadding = 50;
+  var indent = 20;
   var barHeight = 150;
   var barWidth = 40;
   var barGap = 50;
   var topBarPadding = 100;
-  var heroColor = 'rgba(255, 0, 0, 1)';
+  var heroColor = colorAlpha(255, 0, 0, 1);
 
-  var results = [];
-  for (var i = 0; i <= names.length - 1; i++) {
-    results.push([times[i], names[i]]);
-  }
-  getSorted(results);
+  ctx.fillStyle = colorAlpha(0, 0, 0, 0.7);
+  ctx.fillRect(110, 20, 420, 270);
+  ctx.fillStyle = colorAlpha(255, 255, 255, 1);
+  ctx.fillRect(100, 10, 420, 270);
+  ctx.font = '16px PT Mono';
+  ctx.fillStyle = colorAlpha(0, 0, 0, 1);
+
+  drawText(['Ура, вы победили!', 'Список результатов:'], leftPadding, topPadding, indent);
+  collectResults();
+  sort(results);
   var maxTime = results[results.length - 1][0];
-
-  for (i = 0; i <= names.length - 1; i++) {
-
-    if (results[i][1].includes('Вы')) {
-      ctx.fillStyle = heroColor;
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + getRandom(1) + ')';
-    }
-    var name = results[i][1];
-    var time = results[i][0];
-    var barX = leftPadding + (barGap + barWidth) * i;
-    var itemHeight = Math.floor(barHeight * time / maxTime);
-    var barY = topBarPadding + (barHeight - itemHeight);
-    var timeInSec = Math.floor(time) + 'мс';
-    var bottomBarPadding = 265;
-
-    ctx.fillRect(barX, barY, barWidth, itemHeight);
-    ctx.fillStyle = 'rgb(0, 0, 0)';
-    ctx.fillText(timeInSec, barX, barY - 10);
-    ctx.fillText(name, barX, bottomBarPadding);
-  }
-  // console.log(names, times, results);
+  drawDiagram(drawBar);
 };
