12226fac6960ce20330751c1bef6eb2d132f0910
Alexandra <kabrenok@gmail.com>
2017-11-29 15:08:48 +0300

    Add files via upload
    Правки в функции подбора волшебника

--- js/setup.js
+++ js/setup.js
@@ -1,39 +1,5 @@
 'use strict';
-var NAMES = [
-  'Иван',
-  'Хуан Себастьян',
-  'Мария',
-  'Кристоф',
-  'Виктор',
-  'Юлия',
-  'Люпита',
-  'Вашингтон'
-];
-var SURNAMES = [
-  'да Марья',
-  'Верон',
-  'Мирабелла',
-  'Вальц',
-  'Онопко',
-  'Топольницкая',
-  'Нионго',
-  'Ирвинг'
-];
-var COAT_COLOR = [
-  'rgb(101, 137, 164)',
-  'rgb(241, 43, 107)',
-  'rgb(146, 100, 161)',
-  'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)'
-];
-var EYES_COLOR = [
-  'black',
-  'red',
-  'blue',
-  'yellow',
-  'green'
-];
+
 // Показываем окно
 var setupWindow = document.querySelector('.setup');
 setupWindow.classList.remove('hidden');
@@ -47,15 +13,34 @@ var similarWizardsQuantity = 4;
 var getRandom = function (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
-var fullNames = NAMES.map(function () {
-  return getRandom(NAMES) + ' ' + getRandom(SURNAMES);
-});
+
 var SimilarWizard = function (index) {
+  var NAMES = [
+    'Иван',  'Хуан Себастьян',  'Мария',  'Кристоф',
+    'Виктор',  'Юлия',  'Люпита',  'Вашингтон'
+  ];
+  var SURNAMES = [
+    'да Марья',  'Верон',  'Мирабелла',  'Вальц',
+    'Онопко',  'Топольницкая',  'Нионго',  'Ирвинг'
+  ];
+  var COAT_COLOR = [
+    'rgb(101, 137, 164)',  'rgb(241, 43, 107)',  'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',  'rgb(215, 210, 55)',  'rgb(0, 0, 0)'
+  ];
+  var EYES_COLOR = [
+    'black',  'red',  'blue',
+    'yellow',  'green'
+  ];
+  var fullNames = NAMES.map(function () {
+    return getRandom(NAMES) + ' ' + getRandom(SURNAMES);
+  });
+
   this.name = fullNames[index];
   this.coatColor = getRandom(COAT_COLOR);
   this.eyesColor = getRandom(EYES_COLOR);
 };
 var placeSimilarWizard = function (wizard) {
+
   var oneSimilarWizard = wizardTemplate.cloneNode(true);
 
   oneSimilarWizard.querySelector('.setup-similar-label').textContent = wizard.name;
@@ -66,9 +51,8 @@ var placeSimilarWizard = function (wizard) {
 };
 
 for (var i = 0; i < similarWizardsQuantity; i++) {
-  var randomWizards = [];
-  randomWizards[i] = new SimilarWizard(i);
-  fragment.appendChild(placeSimilarWizard(randomWizards[i]));
+  var wizard = new SimilarWizard(i);
+  fragment.appendChild(placeSimilarWizard(wizard));
 }
 
 similarWizardBlock.appendChild(fragment);
